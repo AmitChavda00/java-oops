a28b37e0d10c0d25af955ee7f559b47da095b432
Amit Chavda <amit.c@simformsolutions.com>
2022-02-23 12:13:05 +0530

    aded heirarchical inheritance example

---
+++ inheritance/heirarchical-inheritance/Main.java
@@ -0,0 +1,52 @@
+import javax.security.auth.callback.LanguageCallback;
+
+class Car{
+    private int wheels;
+    private int speed;
+
+    public void run(){
+        System.out.println("I can run");
+    }
+
+    public void setWheels(int wheels){
+        this.wheels=wheels;
+    }
+
+    public int getWheels(){
+        return wheels;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+    public void setSpeed(int speed){
+        this.speed=speed;
+    }
+
+}
+
+class RangRover extends Car{
+    public void display(){
+        System.out.println("I  have "+getWheels()+" wheels and I can run at "+getSpeed()+" km/h speed");
+    }
+}
+
+class Lamborghini extends Car{
+    public void display(){
+        System.out.println("I  have "+getWheels()+" wheels and I can run at "+getSpeed()+" km/h speed");
+    }
+}
+public class Main {
+    public static void main(String args[]){
+        RangRover rangRover=new RangRover();
+        rangRover.setWheels(4);
+        rangRover.setSpeed(135);
+        rangRover.display();
+
+        Lamborghini lamborghini=new Lamborghini();
+        lamborghini.setWheels(4);
+        lamborghini.setSpeed(195);
+        lamborghini.display();
+
+    }
+}
